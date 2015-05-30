37a1813494c56ac3a7c1e9e3208aa77b87db6c4d
Pablo de la Pena <p@shedcollective.org>
2015-05-30 14:42:32 +0100

    Tidied up

--- jquery.wide.slide.pack.js
+++ jquery.wide.slide.pack.js
@@ -1,33 +1,11 @@
-/*
-Copyright (c) 2010 HelloPablo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-Author:         Pablo de la Pena (@hellopablo)
-Version:        1.0
-Dated:          16th November 2010
-
-Dependency:     jQuery 1.2.6
-			    			    
-Documentation:  http://jquery.hellopablo.co.uk/wideslide/
-				http://github.com/HelloPablo/WideSlide
-*/
+/**
+ * Author:        Pablo de la Pena (@hellopablo)
+ * Version:       1.0.0
+ * Dated:         16th November 2010
+ * Dependency:    jQuery 1.2.6
+ * Documentation: http://hellopablo.github.io/wideslide/
+ * License:       MIT
+ **/
 (function($){$.fn.wideSlide=function(options){var settings=$.extend({},$.fn.wideSlide.defaults,options);return this.each(function(){var vars={currentSlide:-1,totalSlides:0,kids:Array(),paused:false,started:false};var slider=$(this);var kids=slider.children("div");kids.each(function(){var child=$(this);child.attr('id','wideSlide_'+vars.totalSlides);child.addClass('wideSlide');vars.kids[vars.totalSlides]=child;vars.totalSlides++;});if(settings.pauseOnHover){slider.hover(function(){if(vars.started==false)
 return false;vars.paused=true;slider.find('.wideSlidePause').fadeIn('fast',function(){$(this).fadeOut('slow')});},function(){if(vars.started==false)
 return false;vars.paused=false;slider.find('.wideSlidePlay').fadeIn('fast',function(){$(this).fadeOut('slow')});});}
